b629bf75cd20c88441c24ec66e49682721dde70b
bagama7777 <bakhronov@gmail.com>
2020-05-25 01:39:35 +0500

    added featured section

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+
+
+$('.slider111').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img     src="../images/next.png" alt="next arrow"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img     src="../images/prev.png" alt="prev arrow"></button>',
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      });
+   
+}); 
+
+
+
+
+
