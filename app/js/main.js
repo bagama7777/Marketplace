d319a17bd32483db211c8a52056ea3bc119c16db
bagama7777 <bakhronov@gmail.com>
2020-05-31 01:33:09 +0500

    remade all section

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,39 @@
 $(function () {
 
 
-$('.slider111').slick({
-        nextArrow: '<button class="slick-arrow slick-next"><img     src="../images/next.png" alt="next arrow"></button>',
-        prevArrow: '<button class="slick-arrow slick-prev"><img     src="../images/prev.png" alt="prev arrow"></button>',
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      });
-   
-}); 
+  $(".rate-star").rateYo({
+    rating: 5,
+    readOnly: true,
+    ratedFill: "#ffc000",
+    normalFill: "#ffffff",
+    starWidth: "15px"
+
+  });
+
+  $('.featured__slider').slick({
+    nextArrow: '<span class="lnr lnr-chevron-right slick-arrow slick-next"></span>',
+    prevArrow: '<span class="lnr lnr-chevron-left slick-arrow slick-prev"></span>',
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+  $('.followers__content').slick({
+    nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>',
+    prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    variableWidth: true
+    // centerMode: true
+    // centerPadding: '0px'
+  });
+
+
+
+  var mixer = mixitup('.products__inner-box');
+
+});
 
 
 
