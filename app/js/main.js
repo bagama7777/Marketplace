c5ac05cdd3a7e48dcbd073b6e6fa4e3359e63a98
bagama7777 <bakhronov@gmail.com>
2020-06-06 01:59:45 +0500

    remade all

--- app/js/main.js
+++ app/js/main.js
@@ -1,41 +1,41 @@
-$(function () {
+$(function() {
 
 
-  $(".rate-star").rateYo({
-    rating: 5,
-    readOnly: true,
-    ratedFill: "#ffc000",
-    normalFill: "#ffffff",
-    starWidth: "15px"
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        ratedFill: "#ffc000",
+        normalFill: "#ffffff",
+        starWidth: "15px"
 
-  });
+    });
 
-  $('.featured__slider').slick({
-    nextArrow: '<span class="lnr lnr-chevron-right slick-arrow slick-next"></span>',
-    prevArrow: '<span class="lnr lnr-chevron-left slick-arrow slick-prev"></span>',
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  });
+    $('.featured__slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+        // nextArrow: '<span class="lnr lnr-chevron-right slick-arrow slick-next"></span>',
+        // prevArrow: '<span class="lnr lnr-chevron-left slick-arrow slick-prev"></span>',
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
 
-  $('.followers__content').slick({
-    nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>',
-    prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 2,
-    variableWidth: true
-    // centerMode: true
-    // centerPadding: '0px'
-  });
+    $('.followers__content').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        variableWidth: true
+    });
+
+
+    $('.products__btn').on('click', function() {
+        $('.products__btn').toggleClass('list');
+    });
 
 
 
-  var mixer = mixitup('.products__inner-box');
+    var mixer = mixitup('.products__inner-box');
 
 });
-
-
-
-
-
